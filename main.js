dc6edb848588c7f79a4238f06d01e3ad85bfc1f4
georodrigues.linda <georodrigues.linda@gmail.com>
2025-04-06 21:55:17 +0000

    inicio de lagogica

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
  let bill = 0
- let tipPorcentage = 0
+ let tipPercentage = 0
  let numberOfPeople = 0
+ let buttonSelected = null
 
-    function reciveBillValue(){
+    function reciveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
     
     }
 
-    function receiveOfpeopleValue(){
+    function receiveOfpeopleValue() {
       numberOfPeople = document.querySelector("#people").valueAsNumber
+    }
+
+    function receiveTipPercentageValue(value) {
+    tipPercentage = value / 100
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
     }
