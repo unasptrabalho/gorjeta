e459483cd31323d4c7bc7d9e3167958f1dc20044
georodrigues.linda <georodrigues.linda@gmail.com>
2025-04-06 18:59:46 +0000

    criaçao do jvs

---
+++ main.js
@@ -0,0 +1,12 @@
+ let bill = 0
+ let tipPorcentage = 0
+ let numberOfPeople = 0
+
+    function reciveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
+    
+    }
+
+    function receiveOfpeopleValue(){
+      numberOfPeople = document.querySelector("#people").valueAsNumber
+    }
